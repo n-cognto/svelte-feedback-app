9a43ae44d9a60b172a9a2b7bfe8ca07f7c533825
n-cognto <benardopeter4@gmail.com>
2025-11-05 05:44:01 +0300

    Entry points

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Import the main App component
+import App from './App.svelte';
+
+// Create and mount the Svelte app to the document body
+const app = new App({
+	target: document.body,
+});
+
+export default app;https://youtu.be/k7KQwlr4RNc?si=se8NAKeUkpeHzc_H
