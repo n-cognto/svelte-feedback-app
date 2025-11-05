6e5a4396224686a6a804a01cef0e96145347b76f
n-cognto <benardopeter4@gmail.com>
2025-11-05 05:39:51 +0300

    Feedback list component

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Import the FeedbackItem component
+  import FeedbackItem from './FeedbackItem.svelte'
+
+  // Prop to receive the feedback array from parent component
+  export let feedback = []
+
+  // Debug log to see feedback data (can be removed in production)
+  console.log(feedback)
+</script>
+
+<!-- Loop through each feedback item and render FeedbackItem component -->
+{#each feedback as fb (fb.id) }
+  <FeedbackItem item={fb} on:delete-feedback />
+
+{/each}
