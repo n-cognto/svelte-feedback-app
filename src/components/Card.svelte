9c38c1977ea50a6ee94d6a32b0964a129a8e37d9
n-cognto <benardopeter4@gmail.com>
2025-11-05 05:41:03 +0300

    Card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,15 @@
+<!-- Card component provides a styled container using slots for flexible content -->
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card{
+        background-color: #fff;
+        padding: 40px 50px;
+        position: relative;
+        border-radius: 15px;
+        margin: 20px 0;
+        color: #333;
+    }
+</style>
